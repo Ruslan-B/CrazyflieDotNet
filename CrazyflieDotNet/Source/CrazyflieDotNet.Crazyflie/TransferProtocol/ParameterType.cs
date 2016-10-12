8f94c1dfa33385d99ba202077f029de754b9561f
Ruslan Balanukhin <ruslan.balanukhin@gmail.com>
2016-10-13 01:26:32 +0200

    Regroup ParameterType.

--- CrazyflieDotNet/Source/CrazyflieDotNet.Crazyflie/TransferProtocol/ParameterType.cs
+++ CrazyflieDotNet/Source/CrazyflieDotNet.Crazyflie/TransferProtocol/ParameterType.cs
@@ -5,17 +5,19 @@ namespace CrazyflieDotNet.Crazyflie.TransferProtocol
     [Flags]
     public enum ParameterType : byte
     {
-        ReadOnly = 1 << 6,
         Int8 = 0x00,
         Int16 = 0x01,
         Int32 = 0x02,
         Int64 = 0x03,
-        Float16 = 0x01 | 0x01 << 2,
-        Float32 = 0x02 | 0x01 << 2,
-        Float64 = 0x03 | 0x01 << 2,
-        UInt8 = 0x00 | 0x01 << 3,
-        UInt16 = 0x01 | 0x01 << 3,
-        UInt32 = 0x02 | 0x01 << 3,
-        UInt64 = 0x03 | 0x01 << 3
+        Float = 0x01 << 2,
+        Float16 = 0x01 | Float,
+        Float32 = 0x02 | Float,
+        Float64 = 0x03 | Float,
+        Unsigned = 0x01 << 3,
+        UInt8 = 0x00 | Unsigned,
+        UInt16 = 0x01 | Unsigned,
+        UInt32 = 0x02 | Unsigned,
+        UInt64 = 0x03 | Unsigned,
+        ReadOnly = 1 << 6,
     }
 }
